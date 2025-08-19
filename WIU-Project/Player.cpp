18f5536c9d7e10c5212c441a3f567a279d7595b7
thisiyu99@gmail.com <thisiyu99@gmail.com>
2025-08-19 10:42:40 +0800

    v

--- WIU-Project/Player.cpp
+++ WIU-Project/Player.cpp
@@ -1,1 +1,34 @@
 #include "Player.h"
+Player::Player() {
+	hackingPoints = 0;
+	playerVirus = nullptr;
+}
+Player::~Player() {
+
+}
+void setHackingPoints(int hackingPoints) {
+	this->hackingPoints = hackingPoints;
+}
+int getHackingPoints() {
+	return hackingPoints;
+}
+void earnPoints(int points) {
+	
+}
+void spendPoints(int points) {
+	if (points <= hackingPoints) {
+		hackingPoints -= points;
+	} 
+}
+
+void Player::setInitials()
+{
+	int temp;
+
+	do {
+		std::cout << "Enter your virus (1 - Worm): ";
+		std::cin >> temp;
+	} while (temp < 1 || temp > 1);
+	
+
+}
--- WIU-Project/Player.h
+++ WIU-Project/Player.h
@@ -1,21 +1,29 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include "Virus.h"
+
 class Player
 {
 private:
 	int hackingPoints; // Points for the virus
 	Virus* playerVirus; // Virus that the player has
+
 public:
 	Player();
 	~Player();
-	void setHackingPoints(int points);
-	int getHackingPoints() const;
+
 	void earnPoints(int points);
 	bool spendPoints(int points);
 	void upgradeVirus(int points );
-	void showMenu();
 
+	void showMenu();
+	void setInitials();
+
+	// Getters and Setters
+	int getHackingPoints(void) const;
+
+	void setHackingPoints(int hackingPoints);
 };
 
