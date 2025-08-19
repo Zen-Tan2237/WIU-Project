f49eb8323c64472752b396172e9727ff43c269e6
thisiyu99@gmail.com <thisiyu99@gmail.com>
2025-08-19 11:19:02 +0800

    updated Player class

--- WIU-Project/Player.cpp
+++ WIU-Project/Player.cpp
@@ -6,31 +6,45 @@ Player::Player() {
 Player::~Player() {
 
 }
-void setHackingPoints(int hackingPoints) {
+
+void Player:: setHackingPoints(int hackingPoints) {
 	this->hackingPoints = hackingPoints;
 }
-int getHackingPoints() {
+
+int Player :: getHackingPoints()const {
 	return hackingPoints;
 }
-void earnPoints(int points) {
-	
+
+Virus* Player::getPlayerVirus() const {
+	return playerVirus;
+}
+
+void Player :: earnPoints(int points) {
+	hackingPoints += points;
 }
 bool Player::spendPoints(int points) {
 	if (points <= hackingPoints) {
 		hackingPoints -= points;
 	} 
 }
+
 void Player::showMenu() {
 
 }
-void Player::setInitials()
+void Player::setInitials(Company companyList[])
 {
-	int temp;
-
+	int type;
+	int companyChoice;
 	do {
 		std::cout << "Enter your virus (1 - Worm): ";
-		std::cin >> temp;
-	} while (temp < 1 || temp > 1);
-	
+		std::cin >> type;
+	} while (type < 1 || type > 1);
+	do {
+		std::cout << "Enter the company you want to start at: ";
+		for (int i = 0; i < 50; i++) {
+			std::cout << i + 1 << "." << companyList[i].getName() << std::endl;
+		}
+		std::cin >> companyChoice;
+	} while (companyChoice < 1 || companyChoice > 50);
 
 }
