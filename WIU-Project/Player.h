9bbe1d4f0dbc34958a4e4c83f1112b5001e34603
thisiyu99@gmail.com <thisiyu99@gmail.com>
2025-08-19 09:52:27 +0800

    v

--- WIU-Project/Player.h
+++ WIU-Project/Player.h
@@ -1,5 +1,21 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "Virus.h"
 class Player
 {
+private:
+	int hackingPoints; // Points for the virus
+	Virus* playerVirus; // Virus that the player has
+public:
+	Player();
+	~Player();
+	void setHackingPoints(int points);
+	int getHackingPoints() const;
+	void earnPoints(int points);
+	bool spendPoints(int points);
+	void upgradeVirus(int points );
+	void showMenu();
+
 };
 
