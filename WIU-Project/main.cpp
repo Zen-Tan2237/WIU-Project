2197379f75107db6e731e7da594870b021e18614
Zen <Zen@DESKTOP-OLQKB9H>
2025-08-19 13:51:32 +0800

    updated main.cpp

--- WIU-Project/main.cpp
+++ WIU-Project/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "Game.h"
 
 int main() {
+
+	Game* game = new Game;
+
+	game->initGame();
+	while (true) {
+		game->printInterface();
+		game->doTurn();
+	}
+
+
+	delete game;
+
+
 	std::cout << "Halalworld" << std::endl;
 	return 0;
 }
