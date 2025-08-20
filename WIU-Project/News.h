1403779f700c06a029b9ca2bf814957b8b0e85fd
jeijeiliam <joonkiat0706@gmail.com>
2025-08-20 17:11:21 +0800

    Update News class: - added more random events and some descriptions for them

--- WIU-Project/News.h
+++ WIU-Project/News.h
@@ -5,19 +5,64 @@ class News
 {
 
 private:
-    //std::string randomEvents[3][5]
+    //std::string randomEventHeadlines[4][5] // 1 for company collaborations(virus spread chance increase), 2 for cybersecurity triggerd,
+    //                               //3 for cybersecurity reports, 4 for company destroyed reports (sad stories)
+    //{
+    //    { // company virus spread increase events
+    //        " is having a international collaboration as a way to increase relations.",
+    //        " have been acquired by a parent company Zen.Inc and have started employee crossover program.",
+    //        " started a sustainability program to help on Earths global warming.",
+    //        " have decided to introduce a internship program to help students gain experience in larger and well known companies.",
+    //        " have cooperated in sponsering a charity event to help others in need.",
+    //    },
 
-    //{ // company virus spread increase events
-    //" is having a international collaboration as a way to increase relations.",
-    //" have been acquired by a parent company Zen.Inc and have started employee crossover program.",
-    //" started a sustainability program to help on Earths global warming.",
-    //" have decided to introduce a internship program to help students gain experience in larger and well known companies.",
-    //" have cooperated in sponsering a charity event to help others in need.",
-    //},
-    //
-    //{ // company virus news (trigger
+    //    { // company virus news (trigger cybersecurity)
+    //        "",
+    //        "",
+    //        "",
+    //        "",
+    //        ""
+    //    },
 
+    //    { // cybersecurity news (cybersecurity making progress or losing against virus)
+    //        "",
+    //        "",
+    //        "",
+    //        "",
+    //        "",
+    //    },
 
+    //    { // news of when company die (the bad effects to teach the evil of a compter virus on companies)
+    //         " Shuts Down Overnight After Devastating Cyber Attack",
+    //         " employees were seen crying together after losing their jobs from a computer virus",
+    //         " Collapses After Computer Virus Destroys Payroll and Client Files",
+    //         " Entire Operation Halted as Virus Erases Production Schedules and Supplier Contacts",
+    //         " ",
+    //    }
     //};
+
+    //std::string randomEventsDescriptions[4][5]
+    //{
+    //    {
+
+    //    },
+
+    //    {
+
+    //    },
+
+    //    {
+
+    //    },
+
+    //    {
+    //        " Employees were seen embracing in the parking lot after learning that years of digital records, finances, and projects had been completely wiped out, forcing immediate closure.",
+    //        " emplyee said\"It was so sudden when when our computers all got destroyed at the same time by the virus\""
+    //        " Collapses After Computer Virus Destroys Payroll and Client Files",
+    //        " Without access to systems, shipments, or communications, the company had no choice but to shut its doors indefinitely.",
+    //        " "
+    //    }
+    //};
+
 };
 
