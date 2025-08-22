b51dc9c1d53911ac4e75cf0b70e118ae9e05149b
jeijeiliam <joonkiat0706@gmail.com>
2025-08-22 15:33:51 +0800

    Update Game class: - optimised randomCollabGenerator code to run smoother

--- WIU-Project/Game.cpp
+++ WIU-Project/Game.cpp
@@ -4,6 +4,9 @@
 #include <cmath>
 #include <string>
 #include <iomanip>
+#include <vector>
+#include <random>
+#include "algorithm"
 
 #include "Game.h"
 
@@ -117,52 +120,27 @@ void Game::randomCollabGenerator()
     int eventTrigger = rand() % 100;
     if (eventTrigger >= 80)
     {
-        // check howw many companies can collab
-        int companyCanCollab = 0;
-        for (int i = 0; i < 5; i++)
+        // check how many companies can collab
+        std::vector<int> eligibleCompanies;
+        for (int i = 0; i < 5; ++i)
         {
             if (companies[i]->getInfectedStatus() < 0.7f)
             {
-                companyCanCollab++;
+                eligibleCompanies.push_back(i);
             }
         }
 
         // collabing only runs if 2 or more companies can collab
-        if (companyCanCollab > 1)
+        if (eligibleCompanies.size() >= 2)
         {
-            bool companyACan = false;
-            while (!companyACan)
-            {
-                companyA = rand() % 5;
-                if (companies[companyA]->getInfectedStatus() < 0.7f)
-                {
-                    bool companyBCan = false;
-                    bool BequalA = true;
-                    while (!companyBCan)
-                    {
-                        // check make sure companyB is not A
-                        while (BequalA)
-                        {
-                            companyB = rand() % 5;
-                            if (companies[companyB]->getInfectedStatus() < 0.7f)
-                            {
-                                if (companyA != companyB)
-                                {
-                                    BequalA = false;
-                                    companyBCan = true;
-                                }
-                            }
-                        }
-                    }
+            std::shuffle(eligibleCompanies.begin(), eligibleCompanies.end(), std::default_random_engine(static_cast<unsigned>(std::time(0))));
+            int companyA = eligibleCompanies[0];
+            int companyB = eligibleCompanies[1];
 
-                    // print out and set the finalised companies collabing spread
-                    newZ->companyCollabNews(companies[companyA]->getName(), companies[companyB]->getName());
-                    companies[companyA]->setCollabSpreadWeightIndex(rand() % 4 + 1, companyB);
-                    companies[companyB]->setCollabSpreadWeightIndex(rand() % 4 + 1, companyA);
-
-                    companyACan = true;
-                }
-            }
+            // print out and set the finalised companies collabing spread
+            newZ->companyCollabNews(companies[companyA]->getName(), companies[companyB]->getName());
+            companies[companyA]->setCollabSpreadWeightIndex(rand() % 4 + 1, companyB);
+            companies[companyB]->setCollabSpreadWeightIndex(rand() % 4 + 1, companyA);
         }
     }
 }
