cf7a50ba8d9f1b53d9f13a67cb559b2e481a8ddf
Zen <Zen@DESKTOP-OLQKB9H>
2025-08-25 16:04:18 +0800

    added YIPEEE as main menu music cus why not

--- WIU-Project/SoundController.cpp
+++ WIU-Project/SoundController.cpp
@@ -4,10 +4,11 @@
 #pragma comment(lib, "winmm.lib")
 
 //*************************************
+// Made by Zen! Just using windows APIs nothing special here...
 // USAGE INSTRUCTIONS!
 // Call the function by doing:
-// filePath is 'resources\soundfile.wav' (MUST BE IN WAV FORMAT!!)
-// SoundController::playSound(filePath)
+// filePath is "resources\soundfile.wav" (MUST BE IN WAV FORMAT!!)
+// SoundController::playSound(L"resources\\soundfile.wav");
 // playSound just plays the sound file
 // playSoundLoop plays the sound in a loop
 // stopSound stops the sound from playing
@@ -18,7 +19,7 @@ void SoundController::playSound(const std::wstring& filePath) {
 }
 
 void SoundController::playSoundLoop(const std::wstring& filePath) {
-    PlaySound(filePath.c_str(), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+    PlaySound(filePath.c_str(), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP); // just adds that loop flag
 }
 
 void SoundController::stopSound() {
