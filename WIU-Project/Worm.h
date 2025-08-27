5813ffedde175b19d116ac97397e2633d9433550
thisiyu99@gmail.com <thisiyu99@gmail.com>
2025-08-27 13:54:52 +0800

    changed the minigame and added some functions in worm class

--- WIU-Project/Worm.h
+++ WIU-Project/Worm.h
@@ -2,11 +2,23 @@
 #include "Virus.h"
 #include "Upgrades.h"
 #include <iostream>
-#include <string>
-#include <cstdlib>
-#include <ctime>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+#include<conio.h>
+#include<windows.h>
 class Worm : public Virus
 {
+private:
+	const int row = 10;
+	const int col = 72;
+	bool lose = false;
+	int wormRow = 0;
+	int wormCol = 0;
+	int wormRowPrevious = wormRow;
+	int wormColPrevious = wormCol;
+	char dir = 's';
+	char** board = new char* [row];
 public:
 
 	Worm();
@@ -14,6 +26,10 @@ public:
 
 	void evolve(Upgrades*) override;
 	void miniGame(int &hackingPoints);
+	void makeboard();
+	void printboard();
+	void update();
+	void getInput();
 
 };
 
